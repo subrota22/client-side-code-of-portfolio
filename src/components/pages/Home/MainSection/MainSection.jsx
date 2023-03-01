b384da089d4bbedea31eed2b13c6a22ee1bf0b1a
Subrota Chandra Sarker <itinfobd24@gmail.com>
2023-03-01 21:55:23 +0600

    add all

--- src/components/pages/Home/MainSection/MainSection.jsx
+++ src/components/pages/Home/MainSection/MainSection.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
 
 const MainSection = () => {
     return (
         <>
-             <div className="container" data-aos="fade-up" data-aos-duration="2000">
+            <div className="container" data-aos="fade-up" data-aos-duration="2000">
                 <div className="card my-4 rounded-2 text-white bg-dark rounded-2 p-3">
                     <div className="card-bod">
                         <div className="d-flex flex-column-reverse flex-md-row">
@@ -11,11 +12,11 @@ const MainSection = () => {
                                 <h5 className="card-title">Name: Subrota chandra sarker </h5>
                                 <h5 className="card-title">Profession: Full-stack developer </h5>
                                 <p className="card-text">
-                                    I am a full-stack web developer as you know from my <br/>
-                                    technology skill, I love coding and learning new things <br/>
-                                    not only coding but also other thing link singing, dancing, exercise,<br/>
-                                    sports and so on. I have 3 plus experience in coding, not only<br/>
-                                    this particular language I have other experiences like C, C++, Next.js <br/>
+                                    I am a full-stack web developer as you know from my <br />
+                                    technology skill, I love coding and learning new things <br />
+                                    not only coding but also other thing link singing, dancing, exercise,<br />
+                                    sports and so on. I have 3 plus experience in coding, not only<br />
+                                    this particular language I have other experiences like C, C++, Next.js <br />
                                     but I am not an expert in these languages as I expert in my other technology skills.
                                     "Thanks to visit this website".
                                 </p>
@@ -24,13 +25,17 @@ const MainSection = () => {
                                 </a>
                             </div>
                             <div className="col">
-                                <img src="https://i.ibb.co/02tSMYX/subrota.png" alt="Subrota chandra sarker"
-                                    className="subrotaImage  w-100  rounded-5" />
+                                <PhotoProvider>
+                                    <PhotoView src="https://i.ibb.co/02tSMYX/subrota.png">
+                                        <img src="https://i.ibb.co/02tSMYX/subrota.png" alt="Subrota chandra sarker"
+                                            className="subrotaImage  w-100  rounded-5" />
+                                    </PhotoView>
+                                </PhotoProvider>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>  
+            </div>
         </>
     );
 };
